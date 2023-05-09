12a11b846ea5baddc096c5f3de36e140b7e22650
Kirill Glushkov <kirglushkovv@gmail.com>
2023-05-10 01:56:44 +0300

    new

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,16 +5,15 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC8g9wV8xEfdkLWNE0a3Yc96IaGv1XnGIo",
-  authDomain: "vktest-4bbb6.firebaseapp.com",
-  projectId: "vktest-4bbb6",
-  storageBucket: "vktest-4bbb6.appspot.com",
-  messagingSenderId: "743344934059",
-  appId: "1:743344934059:web:28e09b8a9da196f43ebbee",
-  measurementId: "G-ZP5TP900QT"
+  apiKey: "AIzaSyBbxjcShsFlQn-Dp5TCI2F4DS86OYOAmqM",
+  authDomain: "imagestore-14f4d.firebaseapp.com",
+  projectId: "imagestore-14f4d",
+  storageBucket: "imagestore-14f4d.appspot.com",
+  messagingSenderId: "206520643436",
+  appId: "1:206520643436:web:916789c1cf76ff3a0e0d49"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
